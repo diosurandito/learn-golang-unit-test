314579c2a1566299c09e4381905c807a794bf311
diosurandito <diosurandito@gmail.com>
2021-10-05 10:05:04 +0700

    update: menggagalkan unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,24 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldDio(t *testing.T) {
 	result := HelloWorld("Dio")
 	if result != "Hello Dio" {
 		// unit test failed
-		panic("Result is not Hello Dio")
+		t.Error("Result must be 'Hello Dio'")
 	}
+	fmt.Println("TestHelloWorldDio Done")
+}
+
+func TestHelloWorldDito(t *testing.T) {
+	result := HelloWorld("Dito")
+	if result != "Hello Dito" {
+		// unit test failed
+		t.Fatal("Result must be 'Hello Dito'")
+	}
+	fmt.Println("TestHelloWorldDito Done")
 }
