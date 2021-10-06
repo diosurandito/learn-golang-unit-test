c9053ebab97313dfcd5fcf7f4bb973efd23d0ded
diosurandito <diosurandito@gmail.com>
2021-10-06 10:58:14 +0700

    add: Adding Skip Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,11 +2,20 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("Can not run on Mac OS")
+	}
+	result := HelloWorld("Dio")
+	require.Equal(t, "Hello Dio", result, "Rseult must be 'Hello Dio'")
+}
 
 func TestHelloWorldRequire(t *testing.T) {
 	result := HelloWorld("Dio")
