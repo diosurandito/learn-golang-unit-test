dd70f13ee4cba65088c8327460f41250f9003620
diosurandito <diosurandito@gmail.com>
2021-10-06 11:21:22 +0700

    add: Before and After Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestMain(m *testing.M) {
+	// before
+	fmt.Println("BEFORE UNIT TEST")
+
+	m.Run() // eksekusi semua unit test
+
+	// after
+	fmt.Println("AFTER UNIT TEST")
+
+}
+
 func TestSkip(t *testing.T) {
 	if runtime.GOOS == "darwin" {
 		t.Skip("Can not run on Mac OS")
